fa3a449c88e1c730deafb663b4d88156e3a75a7e
Hyunseo Yoon <hyunseo021026@naver.com>
2024-10-12 23:51:49 +0900

    Titanic Analysis Submission-hyunseo

--- titanic dataset analysis2.py
+++ titanic dataset analysis2.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 #타이타닉 데이터 불러오기
-data_titanic=pd.read_csv(r'C:\Users\82108\OneDrive\titanic dataset.csv') 
+data1=pd.read_csv(r"C:\Users\82108\OneDrive\gender_submission.csv")
+data2=pd.read_csv(r"C:\Users\82108\OneDrive\test.csv")
+data3=pd.read_csv(r"C:\Users\82108\OneDrive\train.csv")
+data_titanic = pd.concat([data1, data2, data3], ignore_index=True)
 print("titanic data:\n", data_titanic.head())  # data_titanic 확인하기
 
-#열선택 20세 이상인 사람 선택
+#열선택 
 data_titanic.loc[:,['Sex','Age','Survived','SibSp','Embarked']].head() 
 
 #나이가 20세 이상인 사람 선택
@@ -24,7 +26,6 @@ print("Queenstown에서 승선한 사람:\n", person_embarked)
 #새로운 열 추가하기
 bins=[0,19,100] # 나이 구간 설정(0~19:child, 20~100:adult)
 labels=['Child','Adult']
-
 data_titanic['Age_category']=pd.cut(data_titanic['Age'],bins=bins,labels=labels,right=True )
 print(data_titanic[['Age','Age_category']].head(10)) #10번째 행까지 확인
 
@@ -32,16 +33,19 @@ print(data_titanic[['Age','Age_category']].head(10)) #10번째 행까지 확인
 print("각 열의 결측치 확인:")
 print(data_titanic.isnull().sum()) #각 열에서의 결측치 확인
 
-grouped_missing= data_titanic.groupby('Age_category').apply(lambda x: x.isnull().sum())
-print("\n child와 Adult 그룹별 결측치 확인:\n", grouped_missing)
-
 #결측치 많은 데이터 삭제
 data_titanic.drop(columns=['Cabin'],inplace=True) 
 
 #결측치를 평균값으로 대체
-data_titanic['Age'].fillna(data_titanic['Age'].mean(),inplace=True) #fillna()함수를 이용해 결측치를 평균값으로 대체함
-data_titanic['Embarked'].fillna(data_titanic['Embarked'].mode()[0],inplace=True)#embarked는 평균값을 이용할 수 없으므로 최빈값 사용
-print("\n결측치 처리 후:")
+data_titanic['Survived'].fillna(data_titanic['Survived'].mean(),inplace=True)
+data_titanic['Pclass'].fillna(data_titanic['Pclass'].mean(),inplace=True)
+data_titanic['Name'].fillna(data_titanic['Name'].mode()[0],inplace=True)
+data_titanic['Age'].fillna(data_titanic['Age'].mean(),inplace=True)
+data_titanic['SibSp'].fillna(data_titanic['SibSp'].mean(),inplace=True)
+data_titanic['Ticket'].fillna(data_titanic['Ticket'].mode()[0],inplace=True)
+data_titanic['Fare'].fillna(data_titanic['Fare'].mean(),inplace=True)
+data_titanic['Parch'].fillna(data_titanic['Parch'].mean(),inplace=True) #fillna()함수를 이용해 결측치를 평균값으로 대체함
+data_titanic['Embarked'].fillna(data_titanic['Embarked'].mode()[0],inplace=True)
 print(data_titanic.isnull().sum())
 
 #age와 fare의 히스토그램
@@ -54,7 +58,6 @@ plt.title('Age distribution',fontsize=15)
 plt.xlabel('Age')
 plt.ylabel('count')
 
-
 #fare 히스토그램
 plt.subplot(1,2,2)
 plt.hist(data_titanic['Fare'], bins=40, color='lightgreen', edgecolor='black')
@@ -66,9 +69,8 @@ plt.show()
 
 #성별에 따른 생존율 산점도
 survival_rate_by_gender = data_titanic.groupby('Sex')['Survived'].mean().reset_index()
-plt.figure(figsize=(10,10))
-sns.scatterplot(x='Sex', y='Survived', data=survival_rate_by_gender, s=100, color='blue')
-plt.title('Survival Rate by Gender')
+sns.scatterplot(x='Sex', y='Survived', data=survival_rate_by_gender,s=60, color='blue')
+plt.title('Survival Rate by Gender', fontsize=15) 
 plt.ylabel('Survival Rate')
 plt.xlabel('Gender')
 plt.ylim(0,1)
